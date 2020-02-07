44a5f3349aa3b02b036a4345fd8ce8baa6abe243
pantavishnuvardhanreddy <60742077+pantavishnuvardhanreddy@users.noreply.github.com>
2020-02-07 19:21:59 +0530

    Add files via upload

---
+++ count total zeros and ones in a binary number..py
@@ -0,0 +1,16 @@
+#Write a program to count total zeros and ones in a binary number.
+x=int(input('enter the number :'))
+y=(bin(x))
+print(y)
+y=list((y[2:]))
+print(y)
+print("The Number of zeroes in binary number is :",y.count('0'))
+print("The Number of one's in binary number is :",y.count('1'))
+
+
+
+
+
+#z=format(y,'08b')
+
+
